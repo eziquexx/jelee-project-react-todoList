f6216d162c51e86d60a3858fae7eb13abf8f6ff9
jelee <waftyann@gmail.com>
2024-08-05 17:20:08 +0900

    todolist 작업 - 목록 추가 작업 중

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styled, { css, useTheme } from 'styled-components'
+import TodoListInput from './components/TodoListInput';
+
 
 function App() {
+  // todo list 담을 배열
+  const [todoList, setTodoList] = useState( [] );
+
+  // 리스트 담을 상태변수 생성
+  const [listText, setListText] = useState('');
+  const changeListText = (e) => { setListText(e.target.value) };
+
+  const addList = (e) => {
+    e.preventDefault(); // 새로고침 방지
+
+    // 새로운 todo id 생성
+    const newTodoId = todoList.length > 0 ? Math.max(...todoList.map(list => list.id)) + 1 : 1;
+
+    // 새로운 List text 생성
+    const newTodoList = { id: newTodoId, text: listText };
+    setTodoList([...todoList, newTodoList])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <form onSubmit={ addList }>
+          <fieldset>
+            <legend>Add</legend>
+            <input 
+              type='text' 
+              value={ listText } 
+              placeholder='입력해주세요' 
+              onChange={ changeListText }
+            ></input>
+            <button>추가</button>
+          </fieldset>
+        </form>
+      </div>
+
+      <div>
+        <form>
+          <fieldset>
+            <legend>List</legend>
+            <ul>
+              {
+                todoList.map( (list, index) => {
+                  return (
+                    <TodoListInput id={list.id} text={list.text} />
+                  )
+                } )
+              }
+            </ul>
+          </fieldset>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoListInput.jsx
@@ -0,0 +1,7 @@
+function TodoListInput(props) {
+  return (
+      <li key={props.id}>{props.id}.{props.text}<button>삭제</button></li>
+  );
+}
+
+export default TodoListInput;
